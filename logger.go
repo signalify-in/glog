f3105a44745a0ee6282c63582093d0d3e0eba407
Prasanna <Prasanna>
2021-07-31 22:54:41 +0530

    formatted log

--- logger.go
+++ logger.go
@@ -167,7 +167,7 @@ func (l *Logger) getLogStr(level Level, args ...interface{}) string {
 	now := time.Now().Format("2006.01.02 15:04:05")
 	str := fmt.Sprintf("%v", args...)
 	write := ""
-	println("using prefix ", l.LogPrefix)
+	fmt.Printf("using prefix %v\n", l.LogPrefix)
 	if l.LogPrefix == "" {
 		write = fmt.Sprintf("[%s] %s %s \n", now, level, str)
 	} else {
